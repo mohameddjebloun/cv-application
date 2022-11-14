0c6bd0c3958251ac00e8659948e8a349cd33a121
Mohamed Djebloun <djeblounm@gmail.com>
2022-11-14 04:54:40 +0100

    Create functions that test regexes for input

--- src/components/CvForm/GeneralInformation/GeneralInformation.js
+++ src/components/CvForm/GeneralInformation/GeneralInformation.js
@@ -1,2 +1,9 @@
-const GeneralInformation = (props) => {};
+const isEmail = (value) =>
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+const isNotEmpty = (value) => value.trim() !== "";
+const isPhoneNumber = (value) =>
+    /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(value);
+const GeneralInformation = (props) => {
+    return <div className={props.wrapperClass}></div>;
+};
 export default GeneralInformation;
