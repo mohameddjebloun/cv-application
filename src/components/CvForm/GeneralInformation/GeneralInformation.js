26108e43fb47ba7dab700b00ff53180f7be788f9
Mohamed Djebloun <djeblounm@gmail.com>
2022-11-14 06:07:33 +0100

    Add the required input validation

--- src/components/CvForm/GeneralInformation/GeneralInformation.js
+++ src/components/CvForm/GeneralInformation/GeneralInformation.js
@@ -1,9 +1,66 @@
+import useInput from "../../../hooks/use-input";
 const isEmail = (value) =>
     /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
 const isNotEmpty = (value) => value.trim() !== "";
 const isPhoneNumber = (value) =>
     /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(value);
 const GeneralInformation = (props) => {
+    const {
+        value: firstNameValue,
+        isValid: firstNameIsValid,
+        hasError: firstNameHasError,
+        valueChangeHandler: firstNameChangeHandler,
+        inputBlurHandler: firstNameBlurHandler,
+        reset: resetFirstName,
+    } = useInput(isNotEmpty);
+    const {
+        value: lastNameValue,
+        isValid: lastNameIsValid,
+        hasError: lastNameHasError,
+        valueChangeHandler: lastNameChangeHandler,
+        inputBlurHandler: lastNameBlurHandler,
+        reset: resetLastName,
+    } = useInput(isNotEmpty);
+    const {
+        value: addressValue,
+        isValid: addressIsValid,
+        hasError: addressHasError,
+        valueChangeHandler: addressChangeHandler,
+        inputBlurHandler: addressBlurHandler,
+        reset: resetAddress,
+    } = useInput(isNotEmpty);
+    const {
+        value: phoneNumberValue,
+        isValid: phoneNumberIsValid,
+        hasError: phoneNumberHasError,
+        valueChangeHandler: phoneNumberChangeHandler,
+        inputBlurHandler: phoneNumberBlurHandler,
+        reset: resetPhoneNumber,
+    } = useInput(isPhoneNumber);
+    const {
+        value: emailValue,
+        isValid: emailIsValid,
+        hasError: emailHasError,
+        valueChangeHandler: emailChangeHandler,
+        inputBlurHandler: emailBlurHandler,
+        reset: resetEmail,
+    } = useInput(isEmail);
+    const {
+        value: descriptionValue,
+        isValid: descriptionIsValid,
+        hasError: descriptionHasError,
+        valueChangeHandler: descriptionChangeHandler,
+        inputBlurHandler: descriptionBlurHandler,
+        reset: resetDescription,
+    } = useInput(isNotEmpty);
+    const {
+        value: imageValue,
+        isValid: imageIsValid,
+        hasError: imageHasError,
+        valueChangeHandler: imageChangeHandler,
+        inputBlurHandler: imageBlurHandler,
+        reset: resetImage,
+    } = useInput(isNotEmpty);
     return <div className={props.wrapperClass}></div>;
 };
 export default GeneralInformation;
