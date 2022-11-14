aae1a5ec2e8157d89431763f2424641591fd899c
Mohamed Djebloun <djeblounm@gmail.com>
2022-11-14 07:24:53 +0100

    Write the GeneralInformation input jsx

--- src/components/CvForm/GeneralInformation/GeneralInformation.js
+++ src/components/CvForm/GeneralInformation/GeneralInformation.js
@@ -87,6 +87,114 @@ const GeneralInformation = (props) => {
     const imageClasses = imageHasError
         ? `${props.inputClass} ${props.optionalClass}`
         : props.inputClass;
-    return <div className={props.wrapperClass}></div>;
+    //Return the jsx
+    return (
+        <div className={props.wrapperClass}>
+            <div className={firstNameClasses}>
+                <label htmlFor="firstName">First Name</label>
+                <input
+                    type="text"
+                    id="firstName"
+                    value={firstNameValue}
+                    onChange={firstNameChangeHandler}
+                    onBlur={firstNameBlurHandler}
+                />
+                {firstNameHasError && (
+                    <p className={props.errorClass}>
+                        Please enter a first name.
+                    </p>
+                )}
+            </div>
+            <div className={lastNameClasses}>
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                    type="text"
+                    id="lastName"
+                    value={lastNameValue}
+                    onChange={lastNameChangeHandler}
+                    onBlur={lastNameBlurHandler}
+                />
+                {lastNameHasError && (
+                    <p className={props.errorClass}>
+                        Please enter a last name.
+                    </p>
+                )}
+            </div>
+            <div className={addressClasses}>
+                <label htmlFor="address">Address</label>
+                <input
+                    type="text"
+                    id="address"
+                    value={addressValue}
+                    onChange={addressChangeHandler}
+                    onBlur={addressBlurHandler}
+                />
+                {addressHasError && (
+                    <p className={props.errorClass}>Please enter an address.</p>
+                )}
+            </div>
+            <div className={phoneNumberClasses}>
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input
+                    type="text"
+                    id="phoneNumber"
+                    value={phoneNumberValue}
+                    onChange={phoneNumberChangeHandler}
+                    onBlur={phoneNumberBlurHandler}
+                />
+                {phoneNumberHasError && (
+                    <p className={props.errorClass}>
+                        Please enter a valid phone number.
+                    </p>
+                )}
+            </div>
+            <div className={emailClasses}>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={emailValue}
+                    onChange={emailChangeHandler}
+                    onBlur={emailBlurHandler}
+                />
+                {emailHasError && (
+                    <p className={props.errorClass}>
+                        Please enter a valid email.
+                    </p>
+                )}
+            </div>
+            <div className={descriptionClasses}>
+                <label htmlFor="description">Description</label>
+                <input
+                    type="text"
+                    id="description"
+                    value={descriptionValue}
+                    onChange={descriptionChangeHandler}
+                    onBlur={descriptionBlurHandler}
+                />
+                {descriptionHasError && (
+                    <p className={props.errorClass}>
+                        Please enter a description.
+                    </p>
+                )}
+            </div>
+            <div className={imageClasses}>
+                <label htmlFor="image">Image</label>
+                <input
+                    type="file"
+                    id="image"
+                    accept="image/*"
+                    value={imageValue}
+                    onChange={imageChangeHandler}
+                    onBlur={imageBlurHandler}
+                />
+                {imageHasError && (
+                    <p className={props.errorClass}>
+                        Please upload an image file.
+                    </p>
+                )}
+            </div>
+        </div>
+    );
 };
 export default GeneralInformation;
