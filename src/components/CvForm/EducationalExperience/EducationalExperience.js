09bfe9324830fe25bca48ceae5e91b48f7086b05
Mohamed Djebloun <djeblounm@gmail.com>
2022-11-16 01:41:20 +0100

    Add the required input validation

--- src/components/CvForm/EducationalExperience/EducationalExperience.js
+++ src/components/CvForm/EducationalExperience/EducationalExperience.js
@@ -1,2 +1,58 @@
-const EducationalExperience = (props) => {};
+//Imports
+import useInput from "../../../hooks/use-input";
+import Input from "../../UI/Input";
+//Validation functions
+const isNotEmpty = (value) => value.trim() !== "";
+//Component function
+const EducationalExperience = (props) => {
+    //Input validation
+    const {
+        value: universityValue,
+        isValid: universityIsValid,
+        hasError: universityHasError,
+        valueChangeHandler: universityChangeHandler,
+        inputBlurHandler: universityBlurHandler,
+        reset: resetUniversity,
+    } = useInput(isNotEmpty);
+    const {
+        value: cityValue,
+        isValid: cityIsValid,
+        hasError: cityHasError,
+        valueChangeHandler: cityChangeHandler,
+        inputBlurHandler: cityBlurHandler,
+        reset: resetCity,
+    } = useInput(isNotEmpty);
+    const {
+        value: degreeValue,
+        isValid: degreeIsValid,
+        hasError: degreeHasError,
+        valueChangeHandler: degreeChangeHandler,
+        inputBlurHandler: degreeBlurHandler,
+        reset: resetDegree,
+    } = useInput(isNotEmpty);
+    const {
+        value: subjectValue,
+        isValid: subjectIsValid,
+        hasError: subjectHasError,
+        valueChangeHandler: subjectChangeHandler,
+        inputBlurHandler: subjectBlurHandler,
+        reset: resetSubject,
+    } = useInput(isNotEmpty);
+    const {
+        value: startDateValue,
+        isValid: startDateIsValid,
+        hasError: startDateHasError,
+        valueChangeHandler: startDateChangeHandler,
+        inputBlurHandler: startDateBlurHandler,
+        reset: resetStartDate,
+    } = useInput(isNotEmpty);
+    const {
+        value: endDateValue,
+        isValid: endDateIsValid,
+        hasError: endDateHasError,
+        valueChangeHandler: endDateChangeHandler,
+        inputBlurHandler: endDateBlurHandler,
+        reset: resetEndDate,
+    } = useInput(isNotEmpty);
+};
 export default EducationalExperience;
