33a22084f445ef02042e075645229881a9278f1b
Mohamed Djebloun <djeblounm@gmail.com>
2022-11-16 02:27:53 +0100

    Write the EducationalExperience jsx

--- src/components/CvForm/EducationalExperience/EducationalExperience.js
+++ src/components/CvForm/EducationalExperience/EducationalExperience.js
@@ -1,6 +1,7 @@
 //Imports
 import useInput from "../../../hooks/use-input";
 import Input from "../../UI/Input";
+import Button from "../../UI/Button";
 //Validation functions
 const isNotEmpty = (value) => value.trim() !== "";
 //Component function
@@ -73,5 +74,89 @@ const EducationalExperience = (props) => {
     const endDateClasses = endDateHasError
         ? `${props.inputClass} ${props.optionalClass}`
         : props.inputClass;
+    //Return the jsx
+    return (
+        <div className={props.wrapperClass}>
+            <Input
+                classes={universityClasses}
+                errorClass={props.errorClass}
+                type="text"
+                id="university"
+                labelValue={"University"}
+                value={universityValue}
+                placeHolder="University"
+                onChange={universityChangeHandler}
+                onBlur={universityBlurHandler}
+                errorText="Please enter a university name."
+                hasError={universityHasError}
+            />
+            <Input
+                classes={cityClasses}
+                errorClass={props.errorClass}
+                type="text"
+                id="city"
+                labelValue={"City"}
+                value={cityValue}
+                placeHolder="City"
+                onChange={cityChangeHandler}
+                onBlur={cityBlurHandler}
+                errorText="Please enter a city name."
+                hasError={cityHasError}
+            />
+            <Input
+                classes={degreeClasses}
+                errorClass={props.errorClass}
+                type="text"
+                id="degree"
+                labelValue={"Degree"}
+                value={degreeValue}
+                placeHolder="Degree"
+                onChange={degreeChangeHandler}
+                onBlur={degreeBlurHandler}
+                errorText="Please enter a degree."
+                hasError={degreeHasError}
+            />
+            <Input
+                classes={subjectClasses}
+                errorClass={props.errorClass}
+                type="text"
+                id="subject"
+                labelValue={"Subject"}
+                value={subjectValue}
+                placeHolder="Subject"
+                onChange={subjectChangeHandler}
+                onBlur={subjectBlurHandler}
+                errorText="Please enter a subject."
+                hasError={subjectHasError}
+            />
+            <Input
+                classes={startDateClasses}
+                errorClass={props.errorClass}
+                type="date"
+                id="startDate"
+                labelValue={"Start Date"}
+                value={startDateValue}
+                placeHolder="Name"
+                onChange={startDateChangeHandler}
+                onBlur={startDateBlurHandler}
+                errorText="Please select a date."
+                hasError={startDateHasError}
+            />
+            <Input
+                classes={endDateClasses}
+                errorClass={props.errorClass}
+                type="date"
+                id="endDate"
+                labelValue={"End Date"}
+                value={endDateValue}
+                placeHolder="Name"
+                onChange={endDateChangeHandler}
+                onBlur={endDateBlurHandler}
+                errorText="Please select a date."
+                hasError={endDateHasError}
+            />
+            <Button>Delete</Button>
+        </div>
+    );
 };
 export default EducationalExperience;
