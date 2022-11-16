51e374ca94d22adde68d60aceddb09bacfaa84af
Mohamed Djebloun <djeblounm@gmail.com>
2022-11-16 01:59:20 +0100

    Add the error paragraph to the Input component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,16 +1,21 @@
 const Input = (props) => {
     return (
-        <div className={props.classes}>
-            <label htmlFor={props.id}>{props.labelValue}</label>
-            <input
-                type={props.type}
-                id={props.id}
-                value={props.value}
-                placeholder={props.placeHolder}
-                onChange={props.onChange}
-                onBlur={props.onBlur}
-            />
-        </div>
+        <>
+            <div className={props.classes}>
+                <label htmlFor={props.id}>{props.labelValue}</label>
+                <input
+                    type={props.type}
+                    id={props.id}
+                    value={props.value}
+                    placeholder={props.placeHolder}
+                    onChange={props.onChange}
+                    onBlur={props.onBlur}
+                />
+            </div>
+            {props.hasError && (
+                <p className={props.errorClass}>{props.errorText}</p>
+            )}
+        </>
     );
 };
 export default Input;
